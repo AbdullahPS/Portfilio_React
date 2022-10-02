78e41b67bc53fa555b38ad1c7777a819ad37ac63
AbdullahPS <abdullahtabbal@outlook.com>
2022-10-02 12:26:10 -0700

    Header Section: Add Download and Lets Talk button and image

--- portfolio/src/components/header/Header.tsx
+++ portfolio/src/components/header/Header.tsx
@@ -1,10 +1,20 @@
 import React from 'react'
 import './header.css'
+import ME from '../../assets/me.png'
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
+<div className="container container__head">
+  <h5>Hello I'm</h5>
+  <h1>Abdullah Tabbal</h1>
+  <h5 className='text-light'>Front-End Developer</h5>
+  <a href= "#" className='btn'>Download CV</a>
+  <a href= "#Contact" className='btn btn-primary'>Let's Talk</a>
+  <div className="me">
+    <img src={ME} className='img' alt ="Image of Abdullah Tabbal"/>
+  </div>
+</div>
+    )
 }
 
 export default Header
