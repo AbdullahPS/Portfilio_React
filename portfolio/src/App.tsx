829500febd67b8c8add9d2a1ba4cfbbe4c76069b
AbdullahPS <abdullahtabbal@outlook.com>
2022-10-02 11:29:38 -0700

    Import all components in App

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import './App.css'
+import About from './components/about/About'
+import Contact from './components/contact/Contact'
+import Footer from './components/footer/Footer'
 import Header from './components/header/Header'
+import Nav from './components/nav/Nav'
+import Services from './components/services/Services'
+import Portfolio from './components/portfolio/Portfolio'
+import Testimonias from './components/testimonias/Testimonias'
 const App = () => {
   return (
+<>
 <Header/>
+<Nav/>
+<About/>
+<Services/>
+<Portfolio/>
+<Testimonias/>
+<Contact/>
+<Footer/>
+
+</>
     )
 }
 
