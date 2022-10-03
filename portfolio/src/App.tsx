f953e7c850bf1c35dffa2af575e980328e17a994
AbdullahPS <abdullahtabbal@outlook.com>
2022-10-02 17:00:22 -0700

    Thsi commit is to change the email

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import './App.css'
+import Header from './components/header/Header'
 const App = () => {
   return (
-    <div>App</div>
-  )
+<Header/>
+    )
 }
 
 export default App
