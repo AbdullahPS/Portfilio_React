2314eb19e8c5a792a4c1dc05d57ba0a10ad4f7b6
AbdullahPS <abdullahtabbal@outlook.com>
2022-10-04 18:59:12 +0300

    Navbar: Add floating navigation bar with functionality

--- portfolio/src/components/nav/Nav.tsx
+++ portfolio/src/components/nav/Nav.tsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import {useState} from 'react'
+import {AiOutlineHome,AiOutlineContacts} from 'react-icons/ai'
+import {SiAboutdotme} from 'react-icons/si'
+import {BsFillChatLeftQuoteFill} from 'react-icons/bs'
 import './nav.css'
 const Nav = () => {
+  const [activeClass,setActiveClass]=useState('#')
+
   return (
-    <div>Nav</div>
+    <nav>
+      <a href='#' className={activeClass==='#'? 'active': ''}><AiOutlineHome onClick={()=>setActiveClass('#')}/></a>
+      <a href='#about' className={activeClass==='about'? 'active': ''}><SiAboutdotme onClick={()=>setActiveClass('about')}/></a>
+      <a href='#testimonias' className={activeClass==='testimonias'? 'active': ''}><BsFillChatLeftQuoteFill onClick={()=>setActiveClass('testimonias')}/></a>
+      <a href='#contact' className={activeClass==='contact'? 'active': ''}><AiOutlineContacts onClick={()=>setActiveClass('contact')}/></a>
+
+    </nav>
   )
 }
 
