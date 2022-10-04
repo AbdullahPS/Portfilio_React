b461fa8b4adaaf5816e9b4c27556f630b1d37e83
AbdullahPS <abdullahtabbal@outlook.com>
2022-10-04 18:59:49 +0300

    Delete Services Component (I aint gonna include it)

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -5,7 +5,6 @@ import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
 import Header from './components/header/Header'
 import Nav from './components/nav/Nav'
-import Services from './components/services/Services'
 import Portfolio from './components/portfolio/Portfolio'
 import Testimonias from './components/testimonias/Testimonias'
 const App = () => {
@@ -14,7 +13,6 @@ const App = () => {
 <Header/>
 <Nav/>
 <About/>
-<Services/>
 <Portfolio/>
 <Testimonias/>
 <Contact/>
