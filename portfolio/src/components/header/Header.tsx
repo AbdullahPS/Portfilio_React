d8e91a76ddba12f27df00b0f362aab13a5ae68cd
AbdullahPS <abdullahtabbal@outlook.com>
2022-10-04 12:33:37 +0300

    Header: Add Downloadable CV

--- portfolio/src/components/header/Header.tsx
+++ portfolio/src/components/header/Header.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import './header.css'
 import ME from '../../assets/me.png'
+import CV from '../../assets/cv.pdf'
 
 const Header = () => {
   return (
-<div className="container container__head">
+<div className="container header__container">
   <h5>Hello I'm</h5>
   <h1>Abdullah Tabbal</h1>
   <h5 className='text-light'>Front-End Developer</h5>
-  <a href= "#" className='btn'>Download CV</a>
+  <a href= {CV} download className='btn'>Download CV</a>
   <a href= "#Contact" className='btn btn-primary'>Let's Talk</a>
   <div className="me">
     <img src={ME} className='img' alt ="Image of Abdullah Tabbal"/>
